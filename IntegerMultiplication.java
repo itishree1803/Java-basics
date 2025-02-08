aa4d80a26f5cfa4e88a1566f07e5fa560f136cc7
itishree1803 <itishree1803@gmail.com>
2025-02-08 22:08:17 +0530

    Add IntegerMultiplication file

---
+++ IntegerMultiplication.java
@@ -0,0 +1,11 @@
+public class IntegerMultiplication {
+
+    public static void main(String[] args)
+    {
+        System.out.println("The result is : ");
+        int Num1 = 48;
+        int Num2 = 17;
+        int multiplication = Num1 * Num2;
+        System.out.println(multiplication);
+    }
+}
