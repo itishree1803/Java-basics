d417917ab358d2422eacbae9c56f89bcfbd31e51
itishree1803 <itishree1803@gmail.com>
2025-02-08 21:48:15 +0530

    Add IntegerSubtraction file

---
+++ IntegerSubtraction.java
@@ -0,0 +1,11 @@
+public class IntegerSubtraction {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Subtraction of these numbers is: ");
+        int Num1 = 789;
+        int Num2 = 68;
+        int subtraction = Num1 - Num2;
+        System.out.println(subtraction);
+    }
+}
