082f49a3d8a3367d1f464a3f1dd63bf447038b6c
itishree1803 <itishree1803@gmail.com>
2025-02-08 21:45:04 +0530

    Add IntegerAddition file

--- IntegerAddition.java
+++ IntegerAddition.java
@@ -1,10 +1,10 @@
-public class IntegerSum {
+public class IntegerAddition {
 
     public static void main(String[] args)
     {
         System.out.println("Sum of these numbers is: ");
-        int Num1 = 3;
-        int Num2 = 5;
+        int Num1 = 38;
+        int Num2 = 57;
         int Num3 = 18;
         int sum = Num1 + Num2 + Num3;
         System.out.println(sum);
