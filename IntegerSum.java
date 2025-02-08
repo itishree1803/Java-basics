48d36b7fcb8944eb2faa6d9eb8a7b2694b00c1ad
itishree1803 <itishree1803@gmail.com>
2025-02-08 21:03:11 +0530

    Add IntegerSum file

---
+++ IntegerSum.java
@@ -0,0 +1,12 @@
+public class IntegerSum {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Sum of these numbers is: ");
+        int Num1 = 3;
+        int Num2 = 5;
+        int Num3 = 18;
+        int sum = Num1 + Num2 + Num3;
+        System.out.println(sum);
+    }
+}
