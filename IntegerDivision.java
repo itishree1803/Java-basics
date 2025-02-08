20dfe67b16c2e922deb8d5375d045e30b6696099
itishree1803 <itishree1803@gmail.com>
2025-02-08 22:31:09 +0530

    Add IntegerDivision file

---
+++ IntegerDivision.java
@@ -0,0 +1,23 @@
+public class IntegerDivision {
+
+    public static void main(String[] args)
+//    {
+//       To obtain a natural number as the answer:
+//
+//       System.out.println("The result is : ");
+//       int Num1 = 108;
+//       int Num2 = 8;
+//       int division = Num1 / Num2;
+//       System.out.println(division);
+//    }
+
+    {
+       // To obtain the actual answer :
+        System.out.println("The result is : ");
+        float Num1 = 108f;
+        float Num2 = 8f;
+        float division = Num1 / Num2;
+        System.out.println(division);
+    }
+
+}
