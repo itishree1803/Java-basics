dab7cd9c32c0ba6892616aa641461a7e2f17394f
Itishri Sahoo <itishree1803@gmail.com>
2025-02-12 11:46:56 +0530

    Updated ExpressionEvaluator file

--- ExpressionEvaluator.java
+++ ExpressionEvaluator.java
@@ -6,30 +6,30 @@ public class ExpressionEvaluator
     {
 //        //Expression 1 : (x - y) / 2
         Scanner sc = new Scanner (System.in);
-//
-//        System.out.print("Enter the value of x : ");
-//        double x = sc.nextDouble();
-//
-//        System.out.print("Enter the value of y : ");
-//        double y = sc.nextDouble();
-//
-//        double result1 = (x-y)/2;
-//        System.out.print("Result of (x-y)/2 = \n" + result1 );
-//
-//        System.out.println("\n-----------------\n");
-//
+
+        System.out.print("Enter the value of x : ");
+        double x = sc.nextDouble();
+
+        System.out.print("Enter the value of y : ");
+        double y = sc.nextDouble();
+
+        double result1 = (x-y)/2;
+        System.out.print("Result of (x-y)/2 = \n" + result1 );
+
+        System.out.println("\n-----------------\n");
+
 //        //Expression 2 : v^2 - u^2
-//        System.out.print("Enter the value of v : ");
-//        double v = sc.nextDouble();
-//
-//        System.out.print("Enter the value of u : ");
-//        double u = sc.nextDouble();
-//
-//        double result2 = (v*v) - (u*u);
-//
-//        System.out.print("Result of v^2 - u^2 = " + result2);
-//
-//        System.out.println("\n-----------------\n");
+        System.out.print("Enter the value of v : ");
+        double v = sc.nextDouble();
+
+        System.out.print("Enter the value of u : ");
+        double u = sc.nextDouble();
+
+        double result2 = (v*v) - (u*u);
+
+        System.out.print("Result of v^2 - u^2 = " + result2);
+
+        System.out.println("\n-----------------\n");
 
         //Expression 3 : a * b - d
         System.out.print("Enter the value of a : ");
