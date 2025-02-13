26244065d15392bfdbc6467c283f8c4647c10be1
Itishri Sahoo <itishree1803@gmail.com>
2025-02-13 23:22:16 +0530

    Updated EquationEvaluator file

--- EquationEvaluator.java
+++ EquationEvaluator.java
@@ -6,6 +6,7 @@ public class EquationEvaluator
     {
         Scanner sc = new Scanner(System.in);
 
+//      Kinematics (Motion) :
         System.out.print("Enter the initial velocity (u) : ");
         double u = sc.nextDouble();
 
@@ -22,5 +23,16 @@ public class EquationEvaluator
         System.out.println("The result of (v^2-u^2)/2as is = \n" + result);
 
         System.out.println("\n-------------------\n");
+
+//        Kinetic Energy :
+        System.out.print("Enter mass in kg (m) : ");
+        double m = sc.nextDouble();
+
+        System.out.print("Enter velocity in m/s (v1) : ");
+        double v1 = sc.nextDouble();
+
+//        Calculating kinetic energy :
+        double KE = 0.5 * m * v1 * v1;
+        System.out.println("Kinetic Energy (KE) = " + KE + " joules");
     }
 }
