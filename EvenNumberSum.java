a82cd1b940649f3eccb374ec18f8408d1b5bbd86
Itishri Sahoo <itishree1803@gmail.com>
2025-02-20 15:21:28 +0530

    Update EvenNumberSum file

--- EvenNumberSum.java
+++ EvenNumberSum.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+public class EvenNumberSum {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the Lower Limit Number : ");
+        int lowerLimit = sc.nextInt();
+        int startingLimit = lowerLimit;
+        System.out.println("Enter the Upper Limit Number : ");
+        int upperLimit = sc.nextInt();
+        
+        int sum = 0;
+        
+        while (lowerLimit <= upperLimit) {
+            if (lowerLimit % 2 == 0) {
+                sum += lowerLimit;
+            }
+            lowerLimit++ ;
+        }
+        System.out.println("The sum the even numbers from " + startingLimit + " to " + upperLimit + " is : " + sum);
+        
+        sc.close();
+    }
+}
