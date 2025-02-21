828d315eff0ac9b8f517eb7c80eb8bf15f3aaed8
Itishri Sahoo <itishree1803@gmail.com>
2025-02-21 14:44:09 +0530

    Update NaturalNumberSum file

--- NaturalNumberSum.java
+++ NaturalNumberSum.java
@@ -1,1 +1,13 @@
+public class NaturalNumberSum {
+    public static void main(String[] args) 
+    {
+        int sum = 0, i = 1;
 
+        do {
+            sum += i;
+            i++;
+        } while (i <= 100);
+
+        System.out.println("The sum of the first 100 natural numbers is : " + sum);
+    }
+}
