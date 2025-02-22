3ef780aa0e8334f59a3008a08ed6c0d6d84fe038
Itishri Sahoo <itishree1803@gmail.com>
2025-02-22 23:59:49 +0530

    Update HelloGuys.java

--- HelloGuys.java
+++ HelloGuys.java
@@ -2,8 +2,8 @@ public class HelloGuys
 {
     public static void main(String[] args)
     {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Hello, World!");
+        for (int i = 1; i <= 5; i++) ;
+            System.out.println("Hello Guys!");
         }
     }
 }
