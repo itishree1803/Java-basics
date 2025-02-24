e29116b4c0305644b1f00450b0cffcb87120866a
Itishri Sahoo <itishree1803@gmail.com>
2025-02-24 10:08:08 +0530

    Add PrintPattern file

--- PrintPattern.java
+++ PrintPattern.java
@@ -1,1 +1,13 @@
-
+public class PrintPattern  
+{
+    public static void main(String[] args) 
+    {
+        int n = 4;
+        for (int i = n; i > 0; i--) {
+             for (int j = 0; j < i; j++){
+                 System.out.print("*");
+             }
+         System.out.println("\n");
+        }
+    }
+}
