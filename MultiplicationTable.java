d2360b1270042e5ab05a3409dbe93d8705a92e6d
Itishri Sahoo <itishree1803@gmail.com>
2025-02-24 12:02:27 +0530

    Add MultiplicationTable file

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+public class MultiplicationTable 
+{
+    public static void main(String[] arguments) 
+    {
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter a number to get its multiplication table : ");
+       long number = sc.nextLong();
+       
+       System.out.println("Here is the multiplication table of " + number + " : ");
+       for (long i = 1; i <= 10; i++) 
+       {
+           System.out.println(number + " x " + i + " = " + (number * i));
+       }
+    }
+}
