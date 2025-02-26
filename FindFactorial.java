dd73bb320e88a2dfa824c24bb03ec90d139d56a6
Itishri Sahoo <itishree1803@gmail.com>
2025-02-26 20:53:03 +0530

    Add FindFactorial file

--- FindFactorial.java
+++ FindFactorial.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+public class FindFactorial 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+      
+        System.out.print("Enter the number to get its Factorial: ");
+        int number = sc.nextInt();
+        sc.close();
+
+        int i = 1;
+        int factorial = 1;
+
+        while (i <= number) {
+            factorial *= i;
+            i++;
+        }
+
+        System.out.println("Factorial of " + number + " is: " + factorial);
+    }
+}
