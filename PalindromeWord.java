6e25823bc9adddd5d9bd841ae0795b2c4c8b9695
Itishri Sahoo <itishree1803@gmail.com>
2025-02-27 23:12:23 +0530

    Add PalindromeWord file

--- PalindromeWord.java
+++ PalindromeWord.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+public class PalindromeWord 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a word : ");
+        String word = scanner.nextLine();
+        scanner.close();
+
+        String reversedWord = "";
+        int i = word.length() - 1;
+
+        do {
+            reversedWord += word.charAt(i);
+            i--;
+        } while (i >= 0);
+
+        if (word.equalsIgnoreCase(reversedWord)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
+    }
+}
