55602c733dc28ae666320e3c8d8dab9a441925e3
Itishri Sahoo <itishree1803@gmail.com>
2025-02-27 21:49:14 +0530

    Add PalindromeNumber file

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
 
+public class PalindromeNumber 
+{
+    public static void main(String[] args) 
+   {
+        Scanner scanner = new Scanner(System.in);
+     
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        scanner.close();
+
+        int originalNum = num;
+        int reversedNum = 0;
+
+        do {
+            int digit = num % 10;
+            reversedNum = reversedNum * 10 + digit;
+            num /= 10;
+        } while (num > 0);
+
+        if (originalNum == reversedNum) {
+            System.out.println(originalNum + " is a palindrome.");
+        } else {
+            System.out.println(originalNum + " is not a palindrome.");
+        }
+    }
+}
